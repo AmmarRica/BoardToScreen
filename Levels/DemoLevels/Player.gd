55c70fb23272cecff5ec42b014375637e9ac7b1b
Ammar Ahmed <ammarcola@gmail.com>
2025-03-03 18:02:19 -0500

    more placeholders?

--- Levels/DemoLevels/Player.gd
+++ Levels/DemoLevels/Player.gd
@@ -11,18 +11,24 @@ func _ready() -> void:
 
 func get_Input():
 	var input_dir = Input.get_vector("left","right","up","down")
-	velocity = input_dir * SPEED
+	velocity = input_dir.normalized() * SPEED
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	get_Input()
-
 	move_and_slide()
 
 # will axdd different directions later
-	
+	var mouse = get_local_mouse_position()
+	var mousy = velocity/SPEED
+	var angle = snappedf(mousy.angle(), PI/4) / (PI/4)
+	angle = wrapi(int(angle), 0 , 8)
+	$AnimatedSprite2D.animation = "walk"+ str(angle)
+		
 	if velocity.length() != 0:
 		$AnimatedSprite2D.play()
 	else :
 		$AnimatedSprite2D.stop()
+		$AnimatedSprite2D.animation = "walk2"
+		$AnimatedSprite2D.frame = 1
 	
